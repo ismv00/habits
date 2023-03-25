414d70255a36eae0223b97c1c5d452d377e6e94c
ismv00 <ismv00@icloud.com>
2023-03-25 00:07:53 -0400

    feat:finalizando as features

---
+++ web/src/components/HabitList.tsx
@@ -0,0 +1,84 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
+import { Check } from "@phosphor-icons/react";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
+import dayjs from "dayjs";
+
+interface HabitListProps {
+  date: Date;
+  onCompletedChanged: (completed: number) => void;
+}
+
+interface HabistInfo {
+  possiblehabits: {
+    id: string;
+    title: string;
+    createa_at: string;
+  }[];
+  completedHabits: string[];
+}
+
+export function HabitList({ date, onCompletedChanged }: HabitListProps) {
+  const [habitsInfo, setHabitsInfo] = useState<HabistInfo>();
+
+  useEffect(() => {
+    api
+      .get("day", {
+        params: {
+          date: date.toISOString(),
+        },
+      })
+      .then((response) => {
+        setHabitsInfo(response.data);
+      });
+  }, []);
+
+  async function handleTooglehabit(habitId: string) {
+    const isHabitAlreadyCompleted =
+      habitsInfo!.completedHabits.includes(habitId);
+    await api.patch(`/habits/${habitId}/toogle`);
+
+    let completedHabits: string[] = [];
+    if (isHabitAlreadyCompleted) {
+      completedHabits = habitsInfo!.completedHabits.filter(
+        (id) => id !== habitId
+      );
+    } else {
+      completedHabits = [...habitsInfo!.completedHabits, habitId];
+    }
+    setHabitsInfo({
+      possiblehabits: habitsInfo!.possiblehabits,
+      completedHabits,
+    });
+
+    onCompletedChanged(completedHabits.length);
+  }
+
+  const isDateinPast = dayjs(date).endOf("day").isBefore(new Date());
+
+  return (
+    <div className="mt-6 flex flex-col gap-3">
+      {habitsInfo?.possiblehabits.map((habit) => {
+        return (
+          <Checkbox.Root
+            key={habit.id}
+            onCheckedChange={() => handleTooglehabit(habit.id)}
+            checked={habitsInfo.completedHabits.includes(habit.id)}
+            disabled={isDateinPast}
+            className="flex items-center gap-3 group"
+          >
+            <div className=" h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]: bg-green-500 group-data-[state=checked]: border-green-500">
+              <Checkbox.Indicator>
+                <Check size={20} className="text-white" />
+              </Checkbox.Indicator>
+            </div>
+
+            <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]: text-zinc-400">
+              {habit.title}
+            </span>
+          </Checkbox.Root>
+        );
+      })}
+    </div>
+  );
+}
